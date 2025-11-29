83f1db8d658ff8e09ed99f2c863b234691e41939
pressdarling <32807920+pressdarling@users.noreply.github.com>
2025-11-29 18:31:07 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# pipe-while-read
+
+A zsh function that saves me approximately 2-5 seconds every time I use it.
+
+[Obligatory](https://xkcd.com/1319/) [xkcd](https://xkcd.com/1205/).
+
+## Installation
+
+With [oh-my-zsh](https://github.com/ohmyzsh/ohmyzsh):
+
+```zsh
+cd $ZSH_CUSTOM
+git clone https://github.com/pressdarling/pipe-while-read
+omz plugin enable pipe-while-read
+```
+
+Otherwise, simply source it/add it/copy-and-paste it into your `.zshrc`.
+
+## Unlicense
+
+You wouldn't steal a software.
+
+See [The Unlicense](./UNLICENSE)
+
---
+++ pipe-while-read.zsh
@@ -0,0 +1,35 @@
+#!/usr/bin/env zsh
+pipe-while-read () {
+	local dry_run=false 
+	local help=false 
+	while [[ $# -gt 0 ]]
+	do
+		case $1 in
+			(-n | --dry-run) dry_run=true 
+				shift ;;
+			(-h | --help) help=true 
+				shift ;;
+			(*) break ;;
+		esac
+	done
+	if [[ $help == true ]] || [[ $# -lt 1 ]]
+	then
+		printf 'Usage: command | pipe-while-read [-n|--dry-run] [-h|--help] <command> [args...]\n'
+		printf '  Reads stdin line by line and executes <command> with each line as argument\n'
+		printf '  -n, --dry-run  Show commands without executing\n'
+		printf '  -h, --help     Show this help\n'
+		printf 'Example: cat file.txt | pipe-while-read -n echo "Processing:"\n'
+		return 0
+	fi
+	local cmd=$1 
+	shift
+	while IFS= read -r line
+	do
+		if [[ $dry_run == true ]]
+		then
+			echo "[DRY RUN] $cmd $@ $line"
+		else
+			"$cmd" "$@" "$line"
+		fi
+	done
+}
